79ecd2cf42fbba70c127ba6840242e00afc00b2b
KumonDev <89820241+ErickAlfonsoA@users.noreply.github.com>
2024-05-23 01:11:01 -0600

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="Taiga Tournament",
-    version="1.0",
-    description="Programa para organizar torneos",
-    executables=[Executable("TaigaTournament.pyw", base="Win32GUI")]
-)
